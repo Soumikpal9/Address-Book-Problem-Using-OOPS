84444f94ee4c2fab15e66f9675c75103884af9e4
Soumikpal9 <soumikpal1997@gmail.com>
2020-09-28 13:05:28 +0530

    Added: Contact Details Added Using Console

--- src/com/cg/Builder.java
+++ src/com/cg/Builder.java
@@ -1,9 +1,38 @@
 package com.cg;
 import java.util.*;
 
+//UC2
 public class Builder {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		AddressBookBuilder addressBook = new AddressBookBuilder();
+		
 		System.out.println("Welome to Address Book!!!");
-		System.out.println("Add contact details here");
+		System.out.println("Enter first name: ");
+		String firstName = sc.nextLine();
+		System.out.println("Enter last name: ");
+		String lastName = sc.nextLine();
+		System.out.println("Enter address: ");
+		String address = sc.nextLine();
+		System.out.println("Enter city: ");
+		String city = sc.nextLine();
+		System.out.println("Enter state: ");
+		String state = sc.nextLine();
+		System.out.println("Enter zip: ");
+		String zipcode = sc.nextLine();
+		System.out.println("Enter phone number: ");
+		sc.nextLine();
+		String phone = sc.nextLine();
+		System.out.println("Enter email: ");
+		String email = sc.nextLine();
+		
+		ContactDetailsBuilder contact = new ContactDetailsBuilder(firstName, lastName, address, city, state, zipcode, phone, email);
+		addressBook.addContact(contact);
+		
+		List<ContactDetailsBuilder> contDet = new ArrayList<ContactDetailsBuilder>();
+		contDet = addressBook.showContact();
+		for(ContactDetailsBuilder i : contDet) {
+			System.out.println(i);
 		}
+	}
 }
